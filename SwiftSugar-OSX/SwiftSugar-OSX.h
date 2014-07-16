0669a0fd2c8b1b21f051ad96fa3bd51b28a20658
Konstantin Koval <konstantin@rocketfarm.no>
2014-07-16 23:19:13 +0200

    change project structure to library

---
+++ SwiftSugar-OSX/SwiftSugar-OSX.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSugar-OSX.h
+//  SwiftSugar-OSX
+//
+//  Created by Konstantin Koval on 16/07/14.
+//  Copyright (c) 2014 SF. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftSugar-OSX.
+FOUNDATION_EXPORT double SwiftSugar_OSXVersionNumber;
+
+//! Project version string for SwiftSugar-OSX.
+FOUNDATION_EXPORT const unsigned char SwiftSugar_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSugar_OSX/PublicHeader.h>
+
+
